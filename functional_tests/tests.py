b336ce541b486296c815a39153d7ff07f9002b6c
jensk <jenskoller@gmx.net>
2017-12-31 12:55:08 +0100

    next commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,9 +31,9 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+    def test_can_start_a_list_for_one_user(self):
 
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -57,6 +57,32 @@ class NewVisitorTest(LiveServerTestCase):
 
 
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         self.fail('Finish the test!')
 
