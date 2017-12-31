f1736b4aa5144d7333f611e969854f233664d820
jensk <jenskoller@gmx.net>
2017-12-31 14:04:01 +0100

    next commit

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name = 'viewlist'),
 #    path('admin/', admin.site.urls),
 ]
